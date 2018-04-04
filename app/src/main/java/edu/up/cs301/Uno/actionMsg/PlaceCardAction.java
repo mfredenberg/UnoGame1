5ef96223acd552a2432752d9d9ad64949d25140c
mfredenberg <fredenbe20@up.edu>
2018-04-03 20:31:23 -0700

    Action Objects

--- app/src/main/java/edu/up/cs301/Uno/actionMsg/PlaceCardAction.java
+++ app/src/main/java/edu/up/cs301/Uno/actionMsg/PlaceCardAction.java
@@ -1,8 +1,15 @@
 package edu.up.cs301.Uno.actionMsg;
 
+import edu.up.cs301.Uno.Card;
+
 /**
  * Created by fredenbe20 on 3/27/2018.
  */
 
 public class PlaceCardAction {
+    private Card card;
+
+    public PlaceCardAction(Card newCard){
+        card = newCard;
+    }
 }
---
+++ app/src/main/java/edu/up/cs301/Uno/actionMsg/SkipTurnAction.java
@@ -0,0 +1,8 @@
+package edu.up.cs301.Uno.actionMsg;
+
+/**
+ * Created by fredenbe20 on 4/3/2018.
+ */
+
+public class SkipTurnAction {
+}
