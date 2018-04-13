300656a8312525fdbc41c225865af68f86ff21d3
jacobsa20 <jacobsa20@up.edu>
2018-04-13 14:28:15 -0700

    commented out null card

--- app/src/main/java/edu/up/cs301/Uno/UnoGameView.java
+++ app/src/main/java/edu/up/cs301/Uno/UnoGameView.java
@@ -231,8 +231,8 @@ public class UnoGameView extends SurfaceView {
         Bitmap wildDrawFour = BitmapFactory.decodeResource(getResources(), R.drawable.wild_draw_four);
         this.cardPics.put("" + Type.WILDDRAW4, wildDrawFour);
         //null card
-        Bitmap nullCard = BitmapFactory.decodeResource(getResources(), R.drawable.nullcard);
-        this.cardPics.put(""+ Type.WILD, nullCard);
+       // Bitmap nullCard = BitmapFactory.decodeResource(getResources(), R.drawable.nullcard);
+       // this.cardPics.put(""+ Type.WILD, nullCard);
         //cover card
         Bitmap coverCard = BitmapFactory.decodeResource(getResources(), R.drawable.nullcard);
 
